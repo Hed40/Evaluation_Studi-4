677fb76ea6a369c91d20a41909705caa14c1ba13
Hed40 <90209761+Hed40@users.noreply.github.com>
2022-06-20 16:28:38 +0200

    Integration modification nom du joueur

--- DUMAINE Cédric - Dynamiser vos sites web avec Javascript/app.js
+++ DUMAINE Cédric - Dynamiser vos sites web avec Javascript/app.js
@@ -3,75 +3,12 @@ let scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
-
-document.querySelector('.btn-roll').addEventListener('click', () => {
-    if (gamePlaying) {
-        //Numéro au hazar
-        let dice = Math.floor(Math.random() * 6) + 1;
-
-        //Afficher le résultat du lancé
-        let diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'Images/dice-' + dice + '.png';
-
-        //Mise à jour du score si le numéro du dé n'est pas egale à 1.
-        if (dice !== 1) {
-            //Ajoute le score
-            roundScore += dice;
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;
-        } else {
-            //Joueur Suivant
-            nextPlayer();
-        }
-    }
-});
-
-
-document.querySelector('.btn-hold').addEventListener('click', () => {
-    if (gamePlaying) {
-        // Ajoute le score courant au score général du joueur
-        scores[activePlayer] += roundScore;
-
-        // Update de l'interface Utilisateur
-        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-
-        // Vérifier si le joueur à gagné la partie
-        if (scores[activePlayer] >= 100) {
-            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-            document.querySelector('.dice').style.display = 'none';
-            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-            gamePlaying = false;
-        } else {
-            //Joueur Suivant
-            nextPlayer();
-        }
-    }
-});
-
-
-function nextPlayer() {
-    //Nouveau Joueur
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    document.querySelector('.dice').style.display = 'block'; // Affiche le Dé n°1
-}
-
-document.querySelector('.btn-new').addEventListener('click', init);
-
-function init() { //init de la partie
+function init() { //init de la partie.
     scores = [0, 0];
     activePlayer = 0;
     roundScore = 0;
     gamePlaying = true;
-
+    NameChange = updateName;
     document.querySelector('.dice').style.display = 'none';
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -86,6 +23,83 @@ function init() { //init de la partie
     document.querySelector('.player-0-panel').classList.add('active');
 }
 
+//Action sur Bouton Roll DICE.
+document.querySelector('.btn-roll').addEventListener('click', () => {
+    audio.play('#audio');
+    if (gamePlaying) {
+        //Numéro au hazar
+        let dice = Math.floor(Math.random() * 6) + 1;
+
+        //Afficher le résultat du lancé.
+        let diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'Images/dice-' + dice + '.png';
+
+        //Mise à jour du score si le numéro du dé n'est pas egale à 1.
+        if (dice !== 1) {
+            //Ajoute le score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //Sinon Joueur Suivant.
+            nextPlayer();
+        }
+    }
+});
+
+//Action sur bouton Hold.
+document.querySelector('.btn-hold').addEventListener('click', () => {
+    
+    if (gamePlaying) {
+        // Ajoute le score courant au score général du joueur.
+        scores[activePlayer] += roundScore;
+
+        // Update de l'interface Utilisateur.
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        // Vérifier si un joueur à gagné la partie.
+        if (scores[activePlayer] >= 10) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none'; // cache le dé.
+       
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+
+        } else {
+            //Joueur Suivant
+            nextPlayer();
+        }
+    }
+    audio.play('#audio');
+});
+
+//Action sur le bouton NewGame
+document.querySelector('.btn-new').addEventListener('click', init);
+document.querySelector('.btn-new').addEventListener('click', NameChange);
+
+function nextPlayer() {
+    //Joueur Suivant
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'block'; // Affiche le Dé n°1
+
+}
+
+function updateName() {
+    
+  let player_1Name = prompt('Enter a Player 1 name');
+  document.querySelector('#name-0').textContent = player_1Name;
+
+
+  let player_2Name = prompt('Enter a Player 2 name');
+  document.querySelector('#name-1').textContent = player_2Name;
+
+}
 
 
 

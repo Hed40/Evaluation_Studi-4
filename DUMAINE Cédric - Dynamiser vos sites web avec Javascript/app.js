85bcbbc17cc0ad044b8227443bf183590b83a131
Hed40 <90209761+Hed40@users.noreply.github.com>
2022-06-28 12:22:40 +0200

    Update app.js

--- DUMAINE Cédric - Dynamiser vos sites web avec Javascript/app.js
+++ DUMAINE Cédric - Dynamiser vos sites web avec Javascript/app.js
@@ -99,7 +99,6 @@ document.querySelector('.btn-hold').addEventListener('click', () => {
         // Vérifier si un joueur à gagné la partie.
         if (scores[activePlayer] >= 100) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none'; // cache le dé.
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
